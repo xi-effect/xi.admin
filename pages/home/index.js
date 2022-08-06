19a4efa74c0daf52b55cbfd6046a1be14787a988
Mr.G3ntl3man <archi3run@gmail.com>
2022-08-07 01:55:46 +0300

    fix: change layout and added component

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import React from "react";
-import { Stack } from "@mui/material";
+import { Stack, Typography } from '@mui/material';
 import { inject, observer } from "mobx-react";
 import Navigation from "kit/Navigation/Navigation";
+import { CardWithLinks } from '../../components/CardWithLinks/CardWithLinks';
+
 
 const Home = inject()(observer(() => (
   (
@@ -21,12 +23,25 @@ const Home = inject()(observer(() => (
           spacing={0}
           sx={{
             width: "100%",
+            height: '100%',
             m: 0,
+            p: '10px 0',
             position: "relative",
-            overflow: "hidden",
+            overflowY: 'scroll',
+            overflowX: 'hidden',
           }}
         >
-          Главная Админки
+          <Typography
+            p={1}
+            width='100%'
+            variant='h5'
+            textAlign='center'
+          >
+            Главная Админки
+          </Typography>
+
+          <CardWithLinks />
+
         </Stack>
       </Navigation>
     </>

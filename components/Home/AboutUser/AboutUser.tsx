029041a2475b90ba36d5b64816eaa6af24202e12
Mr.G3ntl3man <archi3run@gmail.com>
2022-08-10 21:02:34 +0300

    feat: creating component "AboutUser"

---
+++ components/Home/AboutUser/AboutUser.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import {
+  Divider,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+  Typography,
+} from '@mui/material';
+import { AccountBox, FormatListNumbered, Announcement } from '@mui/icons-material';
+import HeaderForAU from './HeaderForAU';
+import SubHeaderForAU from './SubHeaderForAU';
+
+const AboutUser = inject('userSt')(
+  observer(({ userSt }) => {
+    const {
+      settings: { id, username, permissions },
+    } = userSt;
+
+    const access =
+      permissions && permissions.length ? permissions[0].name : 'Недостаточно разрешений';
+
+    return (
+      <Paper
+        elevation={3}
+        sx={{
+          maxWidth: '340px',
+          margin: '10px',
+          padding: '10px',
+          backgroundColor: 'background.main',
+        }}
+      >
+        <List
+          subheader={
+            <Typography pl={2} component='span' color='lightgray' variant='subtitle1'>
+              Информация о пользователе
+            </Typography>
+          }
+        >
+          <ListItem>
+            <ListItemAvatar>
+              <AccountBox color='primary' />
+            </ListItemAvatar>
+            <ListItemText
+              primary={<HeaderForAU text='Пользователь' />}
+              secondary={<SubHeaderForAU text={username} />}
+            />
+          </ListItem>
+          <Divider />
+
+          <ListItem>
+            <ListItemAvatar>
+              <FormatListNumbered color='primary' />
+            </ListItemAvatar>
+            <ListItemText
+              primary={<HeaderForAU text='Идентификатор' />}
+              secondary={<SubHeaderForAU text={id} />}
+            />
+          </ListItem>
+          <Divider />
+
+          <ListItem>
+            <ListItemAvatar>
+              <Announcement color='primary' />
+            </ListItemAvatar>
+            <ListItemText
+              primary={<HeaderForAU text='Разрешения' />}
+              secondary={<SubHeaderForAU text={access} />}
+            />
+          </ListItem>
+        </List>
+      </Paper>
+    );
+  })
+);
+
+export default AboutUser;

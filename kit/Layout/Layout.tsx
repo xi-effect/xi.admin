4f839c99d9811006b568fc28059c6804ff02e7bf
dushnyy <archi3run@gmail.com>
2022-12-07 23:19:30 +0300

    fix: change structure layout

--- kit/Layout/Layout.tsx
+++ kit/Layout/Layout.tsx
@@ -1,19 +1,68 @@
 import React, { ReactNode } from 'react';
 import Head from 'next/head';
+import { Box, Stack, Theme, useMediaQuery } from '@mui/material';
+import { inject, observer } from 'mobx-react';
+import { useRouter } from 'next/router';
+import UserSt from 'store/user/userSt';
+import NotEnoughRights from './NotEnoughRights';
+import Sidebar from '../Sidebar/Sidebar';
 
 export type LayoutT = {
   title: string;
   children: ReactNode;
+  userSt: UserSt;
 };
 
-const Layout: React.FC<LayoutT> = ({ title, children }) => (
-  <>
-    <Head>
-      <title>{title} | xi.admin</title>
-      <meta name='robots' content='noindex' />
-    </Head>
-    {children}
-  </>
+const Layout = inject('userSt')(
+  observer((props) => {
+    const {
+      title,
+      children,
+      userSt: {
+        settings: { sections },
+      },
+    }: LayoutT = props;
+
+    const router = useRouter();
+
+    const md = useMediaQuery((theme: Theme) => theme.breakpoints.up('md'));
+
+    if (
+      (router.pathname === '/qa' && !sections['quality assurance']?.emailing) ||
+      (router.pathname === '/manage-mode' && !sections.super?.['manage mods']) ||
+      (router.pathname === '/manage-mode/files' &&
+        !sections['content management']?.['manage files'])
+    ) {
+      return <NotEnoughRights />;
+    }
+
+    return (
+      <>
+        <Head>
+          <title>{title} | xi.admin</title>
+          <meta name='robots' content='noindex' />
+        </Head>
+
+        <Stack
+          direction='row'
+          alignItems='flex-start'
+          justifyContent='flex-start'
+          sx={{
+            width: '100%',
+            maxWidth: 1257,
+            margin: '0 auto',
+            padding: md ? '65px 10px 120px 10px' : '0',
+          }}
+        >
+          <Sidebar />
+
+          <Box width='100%' m={md ? '-16px 40px -16px 236px' : '70px 0 10px 0'}>
+            {children}
+          </Box>
+        </Stack>
+      </>
+    );
+  })
 );
 
 export default Layout;
--- kit/Sidebar/Sidebar.tsx
+++ kit/Sidebar/Sidebar.tsx
@@ -1,72 +1,104 @@
-import React from 'react';
-import UserSt from 'store/user/userSt';
-import { useRouter } from 'next/router';
-import { inject, observer } from 'mobx-react';
-import { Stack, Box, useMediaQuery, Theme } from '@mui/material';
+import React, { useState } from 'react';
+import { Stack, useMediaQuery, Theme, IconButton, Box, Menu } from '@mui/material';
 import AboutModerator from 'components/Sidebar/AboutModerator';
-import NotEnoughRights from '../Layout/NotEnoughRights';
+import { Menu as MenuIcon } from '@mui/icons-material';
+import Link from 'next/link';
+import Image from 'next/image';
 import Nav from './Nav';
 
-type NavigationT = {
-  userSt: UserSt;
-  children: React.ReactNode;
-};
+const Sidebar = () => {
+  const md = useMediaQuery((theme: Theme) => theme.breakpoints.up('md'));
 
-const Sidebar = inject('userSt')(
-  observer((props) => {
-    const {
-      children,
-      userSt: {
-        settings: { sections },
-      },
-    }: NavigationT = props;
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-    const router = useRouter();
+  return md ? (
+    <Stack
+      sx={{
+        pb: '65px',
+        height: '100%',
+        width: '220px',
+        position: 'fixed',
+      }}
+    >
+      <Nav />
 
-    const md = useMediaQuery((theme: Theme) => theme.breakpoints.up('md'));
+      <AboutModerator />
+    </Stack>
+  ) : (
+    <Stack
+      direction='row'
+      alignItems='center'
+      justifyContent='space-between'
+      sx={{
+        zIndex: 1,
+        width: '100%',
+        p: '10px 20px',
+        height: '60px',
+        position: 'fixed',
+        backdropFilter: 'blur(10px)',
+        backgroundColor: 'rgba(68,90,255,0.2)',
+      }}
+    >
+      <Link href='/home'>
+        <Stack sx={{ cursor: 'pointer' }} direction='row' alignItems='center'>
+          <Image
+            width={30}
+            height={30}
+            quality={100}
+            src='/icons/logo.svg'
+            alt='логотип xi.admin'
+          />
 
-    if (
-      (router.pathname === '/qa' && !sections['quality assurance']?.emailing) ||
-      (router.pathname === '/manage-mode' && !sections.super?.['manage mods']) ||
-      (router.pathname === '/manage-mode/files' &&
-        !sections['content management']?.['manage files'])
-    ) {
-      return <NotEnoughRights />;
-    }
+          <Box ml='10px' fontWeight={500} fontSize='19px'>
+            xi.admin
+          </Box>
+        </Stack>
+      </Link>
 
-    return (
-      <Stack
-        direction='row'
-        alignItems='flex-start'
-        justifyContent='flex-start'
+      <IconButton
+        onClick={(e) => setAnchorEl(e.currentTarget)}
         sx={{
-          width: '100%',
-          maxWidth: 1257,
-          margin: '0 auto',
-          padding: '65px 10px 120px 10px',
+          width: '40px',
+          height: '40px',
+          borderRadius: '7px',
+          bgcolor: 'primary.dark',
+          '&:hover': {
+            bgcolor: 'primary.main',
+          },
         }}
       >
-        <Stack
-          direction={md ? 'column' : 'row'}
-          sx={{
-            zIndex: 1,
-            position: 'fixed',
-            pb: !md ? 0 : '65px',
-            height: !md ? 'auto' : '100%',
-            width: !md ? '100%' : '220px',
-          }}
-        >
+        <MenuIcon sx={{ color: 'grayscale.0' }} />
+      </IconButton>
+
+      <Menu
+        open={!!anchorEl}
+        anchorEl={anchorEl}
+        onClose={() => setAnchorEl(null)}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        sx={{
+          mt: '15px',
+          '& .MuiBackdrop-root': {
+            backgroundColor: 'rgba(0,0,0,0)',
+          },
+          '& .MuiMenu-list': {
+            p: '8px',
+          },
+          '& .MuiPaper-elevation': {
+            boxShadow: '0',
+            borderRadius: '8px',
+            border: '1px solid #E6E6E6',
+          },
+        }}
+      >
+        <Stack width='220px'>
           <Nav />
-
-          <AboutModerator />
         </Stack>
-
-        <Box width='100%' m={md ? '-16px 40px -16px 236px' : '100px 0 0 0'}>
-          {children}
-        </Box>
-      </Stack>
-    );
-  })
-);
+      </Menu>
+    </Stack>
+  );
+};
 
 export default Sidebar;

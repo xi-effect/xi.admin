c7e765f6c279afb5a9f5a847bdfa658607c2a784
dushnyy <archi3run@gmail.com>
2022-12-08 22:32:37 +0300

    unused func

--- utils/copyToClipboard.ts
+++
@@ -1,6 +0,0 @@
-import RootStore from '../store/rootStore';
-
-export const copyToClipboard = (rootStore: RootStore, toClipboard: string) => {
-  rootStore.showSnackbar('Скопировано в буфер обмена.', 'success');
-  navigator.clipboard.writeText(toClipboard);
-};

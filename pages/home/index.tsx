7569c2037634f554a60f392078ffd853cfa4684d
dushnyy <archi3run@gmail.com>
2023-01-01 14:58:10 +0300

    fix: update layout

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -3,7 +3,6 @@ import { Stack } from '@mui/material';
 import Documentation from 'components/home/Documentation';
 import Site from 'components/home/Site';
 import GitHub from 'components/home/GitHub';
-import AdminPanel from 'components/home/AdminPanel';
 import Design from 'components/home/Design';
 import Auxiliary from 'components/home/Auxiliary';
 import BackEndDoc from 'components/home/BackEndDoc';
@@ -19,12 +18,10 @@ const Home = () => (
       <GitHub />
 
       <Stack m='16px' flex='1 1 100%' maxWidth='448px'>
-        <AdminPanel />
-
         <BackEndDoc />
-      </Stack>
 
-      <Auxiliary />
+        <Auxiliary />
+      </Stack>
 
       <Design />
     </Stack>

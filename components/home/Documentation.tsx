fbe2d93aee28b59f7d8c8c05a8f29e2f964ef6eb
niqzart <qwert45hi@yandex.ru>
2023-02-22 01:19:19 +0300

    fix: dynamic env-variable lookup & misc

--- components/home/Documentation.tsx
+++ components/home/Documentation.tsx
@@ -4,6 +4,8 @@ import { Box } from '@mui/material';
 import LinkC from 'kit/common/LinkC';
 import ClipboardInput from 'kit/common/ClipboardInput';
 
+const VARIABLE_NAME: string = 'NEXT_PUBLIC_PASS_READER';
+
 const Documentation = () => (
   <CardLayout>
     <Box component='span' mb='24px' fontSize={24} fontWeight={500}>
@@ -15,7 +17,7 @@ const Documentation = () => (
     <ClipboardInput
       label='Пароль'
       toClipboardLabel='****************************'
-      toClipboard={process.env.NEXT_PUBLIC_PASS_READER}
+      toClipboard={process.env[VARIABLE_NAME]}
     />
 
     <LinkC href='https://docs.xieffect.ru/'>Перейти</LinkC>
